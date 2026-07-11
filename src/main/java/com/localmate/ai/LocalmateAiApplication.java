640c1f0e7c9b92fa262908fef2d60567cfc0cdce
narxkim <narkim0215@gmail.com>
2026-07-11 16:35:31 +0900

    Initialize Spring Boot project

---
+++ src/main/java/com/localmate/ai/LocalmateAiApplication.java
@@ -0,0 +1,13 @@
+package com.localmate.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalmateAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalmateAiApplication.class, args);
+    }
+
+}
